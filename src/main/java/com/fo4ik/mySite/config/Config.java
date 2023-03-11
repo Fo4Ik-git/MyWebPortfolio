ea2ad4582b20e59bc593c44a5afa1ce39459ec5e
Vlad <zinkovskyi.vladyslav@gmail.com>
2023-03-11 12:26:39 +0100

    change logoRepo to logoService and change switch to lamda fun

--- src/main/java/com/fo4ik/mySite/config/Config.java
+++ src/main/java/com/fo4ik/mySite/config/Config.java
@@ -3,8 +3,8 @@ package com.fo4ik.mySite.config;
 import com.fo4ik.mySite.model.Logo;
 import com.fo4ik.mySite.model.Role;
 import com.fo4ik.mySite.model.User;
-import com.fo4ik.mySite.repo.LogoRepo;
 import com.fo4ik.mySite.repo.UserRepo;
+import com.fo4ik.mySite.service.LogoService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.ui.Model;
@@ -16,14 +16,12 @@ public class Config {
 
     private static final Logger log = LoggerFactory.getLogger(Config.class);
 
-
-
     private final UserRepo userRepo;
-    private final LogoRepo logoRepo;
+    private final LogoService logoService;
 
-    public Config(UserRepo userRepo, LogoRepo logoRepo) {
+    public Config(UserRepo userRepo, LogoService logoService) {
         this.userRepo = userRepo;
-        this.logoRepo = logoRepo;
+        this.logoService = logoService;
     }
 
     public void getUserLogo(User user, Model model) {
@@ -31,7 +29,7 @@ public class Config {
             model.addAttribute("user", user);
             if (user != null) {
                 User userFromDb = userRepo.findByUsername(user.getUsername());
-                Logo logo = logoRepo.findByName("logo");
+                Logo logo = logoService.getLogo(user);
                 if (!logo.getPath().equals("")) {
                     model.addAttribute("logo", logo.getPath());
                 }
@@ -41,15 +39,9 @@ public class Config {
                 List<Role> roles = new ArrayList<>(user.getRoles());
                 for (Role role : roles) {
                     switch (role) {
-                        case ADMIN:
-                            model.addAttribute("isAdmin", true);
-                            break;
-                        case USER:
-                            model.addAttribute("isUser", true);
-                            break;
-                        case MODERATOR:
-                            model.addAttribute("isModerator", true);
-                            break;
+                        case ADMIN -> model.addAttribute("isAdmin", true);
+                        case USER -> model.addAttribute("isUser", true);
+                        case MODERATOR -> model.addAttribute("isModerator", true);
                     }
                 }
             }
