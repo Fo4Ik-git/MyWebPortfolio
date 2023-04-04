7fe09c99cf71a56f022381e2302ead2c94b0e052
Vlad <zinkovskyi.vladyslav@gmail.com>
2023-04-04 23:36:55 +0200

    method getLogo v2

--- src/main/java/com/fo4ik/mySite/config/Config.java
+++ src/main/java/com/fo4ik/mySite/config/Config.java
@@ -9,9 +9,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.ui.Model;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Config {
 
     private static final Logger log = LoggerFactory.getLogger(Config.class);
@@ -25,11 +22,16 @@ public class Config {
     }
 
     public void getUserLogo(User user, Model model) {
-        try {
+      /*  try {
             model.addAttribute("user", user);
             if (user != null) {
                 User userFromDb = userService.getUser(user.getUsername());
-                Logo logo = logoService.getLogo(user);
+                Logo logo;
+                try {
+                    logo = logoService.getLogo(user);
+                } catch (Exception e) {
+                    logo = new Logo();
+                }
                 if (!logo.getPath().equals("")) {
                     model.addAttribute("logo", logo.getPath());
                 }
@@ -47,6 +49,36 @@ public class Config {
             }
         } catch (Exception e) {
             System.out.println("Error: " + e.getMessage());
+        }*/
+
+        try {
+            if (user != null) {
+                User userFromDb = userService.getUser(user.getUsername());
+                model.addAttribute("user", userFromDb);
+                Logo logo = new Logo();
+                if (logoService.getLogo(user) != null) {
+                    logo = logoService.getLogo(user);
+                }
+                model.addAttribute("logo", logo.getPath());
+                model.addAttribute("isActive", user.isActive());
+                for (Role role : user.getRoles()) {
+                    switch (role) {
+                        case ADMIN:
+                            model.addAttribute("isAdmin", true);
+                            break;
+                        case USER:
+                            model.addAttribute("isUser", true);
+                            break;
+                        case MODERATOR:
+                            model.addAttribute("isModerator", true);
+                            break;
+                    }
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Error in Config: " + e.getMessage());
         }
     }
 }
+
+
