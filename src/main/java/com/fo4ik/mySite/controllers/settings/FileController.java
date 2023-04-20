8c7f92f3124cab9c0fc413b959f6c73bce3da106
Vlad <zinkovskyi.vladyslav@gmail.com>
2023-04-20 15:05:46 +0200

    add link in settings to page files

--- src/main/java/com/fo4ik/mySite/controllers/settings/FileController.java
+++ src/main/java/com/fo4ik/mySite/controllers/settings/FileController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/settings/file")
+@RequestMapping("/settings/files")
 public class FileController {
 
     private static final String FILE_PAGE = "pages/settings/files";
