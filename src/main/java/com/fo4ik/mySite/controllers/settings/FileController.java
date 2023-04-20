c9e0b439ff6d6f67f4838efeea8aa725af04d843
Vlad <zinkovskyi.vladyslav@gmail.com>
2023-04-20 15:02:28 +0200

    create file controller

---
+++ src/main/java/com/fo4ik/mySite/controllers/settings/FileController.java
@@ -0,0 +1,21 @@
+package com.fo4ik.mySite.controllers.settings;
+
+import com.fo4ik.mySite.model.User;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/settings/file")
+public class FileController {
+
+    private static final String FILE_PAGE = "pages/settings/files";
+
+    @GetMapping("")
+    public String filePage(@AuthenticationPrincipal User user, Model model) {
+        return FILE_PAGE;
+    }
+
+}
